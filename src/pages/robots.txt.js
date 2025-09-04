384afc4e86401a3228d68266c6e13ddcc5bb9510
marlowgate-ops <marlowgate@gmail.com>
2025-09-04 22:22:44 +0900

    Update robots.txt.js

--- src/pages/robots.txt.js
+++ src/pages/robots.txt.js
@@ -3,7 +3,6 @@ export async function GET() {
 `User-agent: *
 Allow: /
 Sitemap: https://blog.marlowgate.com/rss.xml
-Sitemap: https://blog.marlowgate.com/sitemap-index.xml
 `, {
       headers: { "Content-Type": "text/plain; charset=utf-8" }
     }
